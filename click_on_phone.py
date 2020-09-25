134e08e6f90a29af6ac1d144088ae671eb1a806a
Bahram <ba.jannesar@gmail.com>
2020-09-25 04:36:58 +0330

    click on phone show

--- click_on_phone.py
+++ click_on_phone.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import os
 
-chromedriver = "home/mars/Desktop/WebScrapingCoruse/chromedriver"
+chromedriver = "/home/mars/Desktop/WebScrapingCoruse/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 actions = ActionChains(driver)
@@ -14,5 +14,10 @@ actions = ActionChains(driver)
 driver.get('https://bama.ir/car/detail-g97qyd9r-bmw-x4-28-2017')
 
 
-show_phone_number_button = driver.find_element_by_xpath('//*[(@id = "phone-open")]')
+element = driver.find_element_by_xpath('//*[(@id = "phone-open")]')
+element.location_once_scrolled_into_view
+
+show_phone_number_button = driver.find_element_by_css_selector('#phone-open')
 show_phone_number_button.click()
+
+driver.p
